61ec8d7bce3b632f166031f7cb629c323dd306c1
Eric MULLER <e.mul@free.fr>
2018-01-16 00:29:07 +0100

    Questionnaire in progress

--- frontend/src/main/frontend/src/app/questionnaire/questionnaire-list/questionnaire-list.component.ts
+++ frontend/src/main/frontend/src/app/questionnaire/questionnaire-list/questionnaire-list.component.ts
@@ -4,6 +4,9 @@ import {NotifierService} from '../../shared/simple-notifier.service';
 import {Observable} from 'rxjs/Observable';
 import {QuestionnaireService} from '../../api/services/questionnaire.service';
 import {Questionnaire} from '../../api/model/questionnaire.model';
+import {FormControl, Validators} from '@angular/forms';
+import {CategoryService} from '../../api/services/category.service';
+import {Category} from '../../api/model/category.model';
 
 
 @Component({
@@ -14,20 +17,33 @@ import {Questionnaire} from '../../api/model/questionnaire.model';
 export class QuestionnaireListComponent implements OnInit {
 
 
-  public results: Observable<Questionnaire[]>;
+  public questionnaires: Observable<Questionnaire[]>;
+
+  public categories: Observable<Category[]>;
 
   public displayedColumns = ['title', 'description'];
   public dataSource = new MatTableDataSource<Questionnaire>();
 
-  constructor(private questionnaireService: QuestionnaireService, private notifierService: NotifierService) {
+  public categoryControl = new FormControl('', [Validators.required]);
+
+  public animals = [
+    {name: 'Dog', sound: 'Woof!'},
+    {name: 'Cat', sound: 'Meow!'},
+    {name: 'Cow', sound: 'Moo!'},
+    {name: 'Fox', sound: 'Wa-pa-pa-pa-pa-pa-pow!'},
+  ];
+
+  constructor(private questionnaireService: QuestionnaireService, private notifierService: NotifierService,
+              private categorieService: CategoryService) {
   }
 
   ngOnInit() {
-    this.results = this.questionnaireService.getQuestionnaires();
+    this.questionnaires = this.questionnaireService.getQuestionnaires();
+    this.categories = this.categorieService.getCategories();
   }
 
   public search() {
-    this.results = this.questionnaireService.getQuestionnaires();
+    this.questionnaires = this.questionnaireService.getQuestionnaires();
   }
 
 
